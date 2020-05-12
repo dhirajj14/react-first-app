994f7b1072150030085c01b2af8bd42f472d6d87
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-12 09:37:57 -0500

    using setstate in react first app

--- src/index.js
+++ src/index.js
@@ -17,20 +17,21 @@ const Book = ({title, author, pages}) => {
 }
 
 class Library extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      open : true
-    }
+  state = {open : false}
+  toggleOpenClosed = () => {
+    this.setState( prevState => ({
+      open: !prevState.open
+    }))
   }
   render(){
     const {books} = this.props;
     return(
       <div>
         <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+        <input type="Button" onClick={this.toggleOpenClosed} value="Change"></input>
         {books.map(
           (book, i) => <Book key={i} title={book.title} author={book.author} pages={book.pages}/>
-        )}
+        )}   
       </div>
     )  
   }
