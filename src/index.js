4a66aca36fcd79fdc98961d016472c522e65dde0
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-12 07:05:39 -0500

    add title component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+const title = React.createElement(
+  'h1',
+  {id: 'title',  className: 'header', style: style},
+  'Hello World'
+)
+
+ReactDom.render(
+  title,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
