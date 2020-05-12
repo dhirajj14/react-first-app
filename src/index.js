41407d67ad573fa02ae9f70fff1a5cd61302ad68
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-12 09:02:54 -0500

    compose components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,28 @@
 import React, { Component } from 'react'
 import ReactDOM, {render} from 'react-dom'
 
-let skiData = {
-  total: 100,
-  powder: 40,
-  backcountry:20,
-  goal: 200
-}
-
-const getPercent = decimal =>{
-  return decimal * 100 + '%'
-}
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total/goal)
-}
-
-const SkiDayCounter = ({total,powder,backcountry, goal}) =>{
+const Book = ({title, author, pages}) => {
   return(
     <section>
-        <div>
-          <p>Total Days:{total}</p>
-        </div>
-        <div>
-          <p>Powder Days: {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days: {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal Progress: {calcGoalProgress(total,goal)}</p>
-        </div>
-      </section>
+      <h2>{title}</h2>
+      <p>By: {author}</p>
+      <p>Pages: {pages}</p>
+    </section>
+  )
+}
+const Library = () => {
+  return(
+    <div>
+      <Book title="Spark Essential" author="Dhiraj" pages={900}/>
+      <Book title="Web" author="Dhiraj" pages={500}/>
+      <Book title="Embbeded System" author="Dhiraj" pages={300}/>
+    </div>
   )
 }
 
 render(
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}/>,
+  <div>
+    <Library/>
+  </div>,
   document.getElementById('root')
   )
